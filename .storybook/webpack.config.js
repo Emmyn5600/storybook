cf52d0eb7a958c13e0ac8297e6a512fd78347843
emmyn5600 <emmy66418@gmail.com>
2023-07-21 22:43:34 +0200

    initialize a repo and start implementing the storybook

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,22 @@
+// .storybook/webpack.config.js
+const path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
